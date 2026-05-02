ae8e4a770a1be624c7000f33685dd84737512933
granitgashi667-svg <granitgashi667@gmail.com>
2026-05-02 21:51:31 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// Zëvendëso me të dhënat e tua të projektit Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDummyReplaceWithYourRealKey",
+    authDomain: "albfilms24.firebaseapp.com",
+    projectId: "albfilms24",
+    storageBucket: "albfilms24.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abc123"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
