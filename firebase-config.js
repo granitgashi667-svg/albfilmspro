38f8a832e9e9bba35806db24c753c665128437b8
granitgashi667-svg <granitgashi667@gmail.com>
2026-05-03 16:59:51 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,16 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Zëvendëso me të dhënat e tua
+// 🔥 VENDOS KONFIGURIMIN TËND TË VËRTETË TË FIREBASE KËTU!
 const firebaseConfig = {
-    apiKey: "AIzaSyDummyReplaceWithYourRealKey",
-    authDomain: "albfilms24.firebaseapp.com",
-    projectId: "albfilms24",
-    storageBucket: "albfilms24.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abc123"
+    apiKey: "AIzaSyDummyReplaceWithYourRealKey", // <-- VENDOS ÇELËSIN E VËRTETË
+    authDomain: "albfilms24.firebaseapp.com",    // <-- VENDOS DOMAIN-IN E VËRTETË
+    projectId: "albfilms24",                     // <-- VENDOS PROJECT ID-NË E VËRTETË
+    storageBucket: "albfilms24.appspot.com",     // <-- VENDOS BUCKET-IN E VËRTETË
+    messagingSenderId: "123456789",              // <-- VENDOS SENDER ID-NË E VËRTETË
+    appId: "1:123456789:web:abc123"              // <-- VENDOS APP ID-NË E VËRTETË
 };
 
 const app = initializeApp(firebaseConfig);
