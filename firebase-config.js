7e4a7ddecb1d9543b3391cf69a17bfee32d10964
granitgashi667-svg <granitgashi667@gmail.com>
2026-05-03 18:37:52 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,19 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// 🔥 VENDOS KONFIGURIMIN TËND TË VËRTETË TË FIREBASE KËTU!
+// 🔥 VENDOS KONFIGURIMIN TËND PERSONAL TË FIREBASE KËTU!
+// Mund t'i gjeni këto të dhëna në Firebase Console > Project Settings
 const firebaseConfig = {
-    apiKey: "AIzaSyDummyReplaceWithYourRealKey", // <-- VENDOS ÇELËSIN E VËRTETË
-    authDomain: "albfilms24.firebaseapp.com",    // <-- VENDOS DOMAIN-IN E VËRTETË
-    projectId: "albfilms24",                     // <-- VENDOS PROJECT ID-NË E VËRTETË
-    storageBucket: "albfilms24.appspot.com",     // <-- VENDOS BUCKET-IN E VËRTETË
-    messagingSenderId: "123456789",              // <-- VENDOS SENDER ID-NË E VËRTETË
-    appId: "1:123456789:web:abc123"              // <-- VENDOS APP ID-NË E VËRTETË
+    apiKey: "AIzaSyB2...", // Zëvendëso me çelësin e vërtetë
+    authDomain: "albfilmspro.firebaseapp.com", // Zëvendëso me emrin e projektit
+    projectId: "albfilmspro", // Zëvendëso me ID-në e projektit
+    storageBucket: "albfilmspro.appspot.com", // Zëvendëso me storage bucket
+    messagingSenderId: "123456789", // Zëvendëso me sender ID
+    appId: "1:123456789:web:abc123" // Zëvendëso me App ID
 };
 
+// Inicializimi i Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
